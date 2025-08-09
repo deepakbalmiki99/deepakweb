9cd96be4372354d38f7741deabb1e68dbf5fa8cf
Deepak Balmiki <deepakbalmiki@gmail.com>
2025-08-09 01:58:25 +0000

    09-Aug-2025_02

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,16 +10,16 @@ from streamlit.components.v1 import html
 import pathlib
 
 # --- Page config & title
-st.set_page_config(page_title="Embed HTML Demo", layout="centered")
-st.title("Embedding an External HTML File")
+#st.set_page_config(page_title="Embed HTML Demo", layout="centered")
+#st.title("Embedding an External HTML File")
 
 # --- Read your HTML file
 html_path = pathlib.Path(__file__).parent / "page.html"
 raw_html  = html_path.read_text(encoding="utf-8")
 
 # --- Embed!
-# height controls the iframe height, scrolling=True adds scrollbars if needed
-html(raw_html, height=600, scrolling=True)
+#height controls the iframe height, scrolling=True adds scrollbars if needed
+html(raw_html,width=1600, height=1200, scrolling=True)
 
 # --- Fallback link
 # st.markdown(
